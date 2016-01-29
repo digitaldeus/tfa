072c6b2dc8688fa196271704bb9a7e971ced8955
Deus Duke <deusduke@gmail.com>
2016-01-29 03:19:25 -0800

    started styling the home landing page

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'lading#index'
+  root 'landing#index'
   resources :establishments, shallow: true do
     resources :staff do
       member do
