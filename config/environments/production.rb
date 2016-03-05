8df2e9a0c5ddbd72bf20ea5c522c0cacfef29b8e
Deus Duke <deusduke@gmail.com>
2016-03-04 23:38:18 -0800

    added mail host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # mail setup
-  # config.action_mailer.default_url_options = { host: 'thefellowshipapp.com' }
+  config.action_mailer.default_url_options = { host: 'thefellowshipapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.sparkpostmail.com",
