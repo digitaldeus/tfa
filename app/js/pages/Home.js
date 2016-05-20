5b7df0c50a3321b6d408eea170bec31ea6172dd9
Deus Duke <deusduke@gmail.com>
2016-05-20 00:41:19 -0700

    updated image references, added favicons

--- app/js/pages/Home.js
+++ app/js/pages/Home.js
@@ -25,22 +25,22 @@ export default React.createClass({
             <div className="container small-12 text-center medium-text-right">
               <ul className="list-inline">
                 <li className="social-link">
-                  <a href="#"><img src="http://assets.thefellowshipapp.com/assets/landing/icon-facebook-610e9b3b84a5c4ee09a62d2c81b855f6bc66d10630dd898cdd251387e018e475.svg" alt="Icon facebook" /></a>
+                  <a href="#"><img src="/img/landing/icon-facebook.svg" alt="Icon facebook" /></a>
                 </li>
                 <li className="social-link">
-                  <a href="#"><img src="http://assets.thefellowshipapp.com/assets/landing/icon-twitter-f5aaa1e7fcada8f9c2de320e06f09872ece29b19097bf62b23b1db5c10701136.svg" alt="Icon twitter" /></a>
+                  <a href="#"><img src="/img/landing/icon-twitter.svg" alt="Icon twitter" /></a>
                 </li>
                 <li className="social-link">
-                  <a href="#"><img src="http://assets.thefellowshipapp.com/assets/landing/icon-google_plus-9eb657326968a544b95640892e2140fc8ba04ee572e67477c5b04f3d1add8a62.svg" alt="Icon google plus" /></a>
+                  <a href="#"><img src="/img/landing/icon-google_plus.svg" alt="Icon google plus" /></a>
                 </li>
                 <li className="social-link">
-                  <a href="#"><img src="http://assets.thefellowshipapp.com/assets/landing/icon-instagram-b3f5504ad9ae6e3fc82758d16f05ac48e7fd3edd09fd5a369342dfe820ebee5d.svg" alt="Icon instagram" /></a>
+                  <a href="#"><img src="/img/landing/icon-instagram.svg" alt="Icon instagram" /></a>
                 </li>
                 <li className="social-link">
-                  <a href="#"><img src="http://assets.thefellowshipapp.com/assets/landing/icon-youtube-4d73fdff3aa067e7db1df6959084666ec75b408091a444db180638ae1c69196b.svg" alt="Icon youtube" /></a>
+                  <a href="#"><img src="/img/landing/icon-youtube.svg" alt="Icon youtube" /></a>
                 </li>
                 <li className="social-link">
-                  <a href="#"><img src="http://assets.thefellowshipapp.com/assets/landing/icon-linkedin-584f8ab52035d084193277b5b3252f9dfdb9ffbb12c4edbdacab2b7143735688.svg" alt="Icon linkedin" /></a>
+                  <a href="#"><img src="/img/landing/icon-linkedin.svg" alt="Icon linkedin" /></a>
                 </li>
               </ul>
 
@@ -49,7 +49,7 @@ export default React.createClass({
         </div>
         <div className="container">
           <div className="row">
-            <div className="medium-5 columns hide-for-small-only"><img src="http://assets.thefellowshipapp.com/assets/landing/phone-profile-5f8ce25da09459927566832721f6e46f788e3ce13fc0f96abe56a7827b9775a0.png" alt="Phone profile" /></div>
+            <div className="medium-5 columns hide-for-small-only"><img src="/img/landing/phone-profile.png" alt="Phone profile" /></div>
             <div className="medium-7 columns">
               <h2>Extend your church beyond the sanctuary and into the hands of thousands!</h2>
               <strong>YOUR ENTIRE CHURCH RIGHT HERE</strong>
@@ -65,7 +65,7 @@ export default React.createClass({
                 past sermons whenever they like from anywhere in the world.
               </p>
               <div className="text-center medium-text-left">
-                <img src="http://assets.thefellowshipapp.com/assets/landing/Sermons-851d6e7e6b72a4213e41e5e55e18a42ea0963b2cfed685162d08b77f1a3c0c87.png" alt="Sermons" />
+                <img src="/img/landing/Sermons.png" alt="Sermons" />
               </div>
             </div>
           </div>
@@ -94,19 +94,19 @@ export default React.createClass({
                   </p>
                 </div>
                 <div className="large-6 columns show-for-large align-bottom text-right">
-                  <img src="http://assets.thefellowshipapp.com/assets/landing/phone-filter-a8f4893e753a7146b1487c6b1d456cd4b534e4c6804a92f8541991cb05174f1c.png" alt="Phone filter" />
+                  <img src="/img/landing/phone-filter.png" alt="Phone filter" />
                 </div>
               </div>
             </div>
             <div className="medium-5 columns hide-for-small-only">
-              <img src="http://assets.thefellowshipapp.com/assets/landing/phone-map-97a09087a05298ce0f054f2f46e5a3db7ff3a3aa28a39804dff1a865f9b33e48.png" alt="Phone map" />
+              <img src="/img/landing/phone-map.png" alt="Phone map" />
             </div>
           </div>
         </div>
         <div className="bg-darkgrey">
           <div className="container nopad-bottom">
             <div className="row">
-              <div className="small-12 medium-5 columns align-bottom text-center medium-text-left"><img src="http://assets.thefellowshipapp.com/assets/landing/phone-tide-1a5ff1fdd222b6103d888542ec96a32f90b204e6f1b4189750928ce4273ab2b5.png" alt="Phone tide" /></div>
+              <div className="small-12 medium-5 columns align-bottom text-center medium-text-left"><img src="/img/landing/phone-tide.png" alt="Phone tide" /></div>
               <div className="small-12 medium-7 columns">
                 <h2>Save time, save money.Take your giving to the next level.</h2>
                 <strong>NO CASH, NO PROBLEM!</strong>
@@ -129,7 +129,7 @@ export default React.createClass({
         <div className="container">
           <div className="row">
             <figure className="text-center callout medium-4 small-12 columns">
-              <img src="http://assets.thefellowshipapp.com/assets/landing/icon-callout-calendar-e5329e40dad783ba55d666546deb0f1500db8413555c11a05f1a162afd15ae6d.png" alt="Icon callout calendar" />
+              <img src="/img/landing/icon-callout-calendar.png" alt="Icon callout calendar" />
               <figcaption>
                 <p>
                   <strong className="text-uppercase">SYNCHRONIZED CALENDAR</strong>
@@ -141,7 +141,7 @@ export default React.createClass({
               </figcaption>
             </figure>
             <figure className="text-center callout medium-4 small-12 columns">
-              <img src="http://assets.thefellowshipapp.com/assets/landing/icon-callout-convo-e0c53f36c529ec8c4ac8f13eb2c8e7953f6735d4d4db47a6b40cd64d9ddbfaa6.png" alt="Icon callout convo" />
+              <img src="/img/landing/icon-callout-convo.png" alt="Icon callout convo" />
               <figcaption>
                 <p>
                   <strong className="text-uppercase">DIRECT MESSAGING</strong>
@@ -153,7 +153,7 @@ export default React.createClass({
               </figcaption>
             </figure>
             <figure className="text-center callout medium-4 small-12 columns">
-              <img src="http://assets.thefellowshipapp.com/assets/landing/icon-callout-notes-0974ec41a045826c725b60db1be20fd3c1c69cc6b9fb13b7324bafdc720c0221.png" alt="Icon callout notes" />
+              <img src="/img/landing/icon-callout-notes.png" alt="Icon callout notes" />
               <figcaption>
                 <p>
                   <strong className="text-uppercase">NOTES &amp; BOOKMARKS</strong>
