0e12233c74c9c9e9779166ebfa803f39c6995994
Deus Duke <deusduke@gmail.com>
2016-05-20 01:40:30 -0700

    installed react fire

---
+++ app/js/utils/db.js
@@ -0,0 +1,16 @@
+
+
+
+
+// // Set the configuration for your app
+// // TODO: Replace with your project's config object
+// var config = {
+//   apiKey: '<your-api-key>',
+//   authDomain: '<your-auth-domain>',
+//   databaseURL: 'https://the-fellowship-app-dev.firebaseio.com/',
+//   storageBucket: '<your-storage-bucket>'
+// };
+// firebase.initializeApp(config);
+
+// // Get a reference to the database service
+// var database = firebase.database();
