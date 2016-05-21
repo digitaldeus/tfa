f1c278e8c54f6c706b81695832f0e9c8a5dca0b1
Deus Duke <deusduke@gmail.com>
2016-05-21 00:23:36 -0700

    abstracted out search from location search for reuse

--- app/js/components/Search.js
+++ app/js/components/Search.js
@@ -1,7 +1,63 @@
 import React from 'react'
 
+const Search = React.createClass({
+  getInitialState: function () {
+    return {
+      predictions: [],
+      selectedPlace: null
+    };
+  },
+  selectPlace: function (location) {
+    this.setState(Object.assign({}, this.state, {
+      selectedPlace: location,
+      predictions: []
+    }));
+
+    this.refs.searchInput.value = location.description;
+  },
+  updatePredictions: function (searchString) {
+    if (searchString && typeof searchString === "string" && searchString.length > 1) {
+      let predictions = [];
+      // run the passed in autocomplete function
+      this.props.onAutoComplete(searchString, (predictions) => {
+        this.setState(Object.assign({}, this.state, {predictions}));
+      });
+    } else {
+      this.removePredictions();
+    }
+  },
+  removePredictions: function () {
+    this.setState(Object.assign({}, this.state, {
+      predictions: [],
+      selectedPlace: null
+    }));
+  },
+  render: function () {
+    return (
+      <div className={this.props.name + '-container'}>
+        <input className={this.props.name} id={this.props.name} placeholder={this.props.placeholder}
+          onChange={e => this.updatePredictions(e.target.value) }
+          onFocus={e => {
+            this.updatePredictions(e.target.value);
+          } }
+          onBlur={e => this.removePredictions() }
+          ref="searchInput" />
+        <ul className="search-result-container">
+          {
+            this.state.predictions.map(p => (
+              <li key={p.place_id}
+                className={`search-result #{this.props.name}-result`}
+                onMouseDown={e => this.selectPlace(p) }>{p.description}</li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+});
+
 export const ChurchSearch = React.createClass({
-  render: function() {
+  render: function () {
     return (
       <div className="church-search-container">
         <input class="church-search" id="church-search" placeholder="Search for a church" />
@@ -11,75 +67,29 @@ export const ChurchSearch = React.createClass({
 });
 
 export const LocationSearch = React.createClass({
-  getInitialState: function() {
-    return {
-      predictions: [],
-      selectedLocation: null
-    };
+  doAutoComplete: function (input, callback) {
+    const service = new google.maps.places.AutocompleteService();
+    service.getPlacePredictions({
+      input: input, types: ['(cities)']
+    }, (predictions, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        callback(predictions.map(l => {
+          const desc = l.terms.map(term => term.value);
+          return {
+            description: desc.splice(0, desc.length - 1).join(','),
+            place_id: l.place_id
+          };
+        }));
+      }
+    });
   },
-  selectLocation: function(location) {
-    this.setState(Object.assign({}, this.state, {
-      selectedLocation: location,
-      predictions: []
-    }));
-    
-    this.refs.searchInput.value = location.description;
-  },
-  updatePredictions: function(searchString) {
-    if (searchString && typeof searchString === "string" && searchString.length > 1) {
-      let predictions = [];
-      
-      doAutoComplete(searchString, (predictions, status) => {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          this.setState(Object.assign({}, this.state, {
-            predictions: predictions.map(l => {
-              const desc = l.terms.map(term => term.value);
-              return {
-                description: desc.splice(0, desc.length - 1).join(','),
-                place_id: l.place_id
-              };
-            })
-          }))
-        }
-        else {
-          console.error("Couldn't get predictions for location, status was: ", status);
-        }
-      })
-    } else {
-      this.removePredictions();
-    }
-  },
-  removePredictions: function() {
-    this.setState(Object.assign({}, this.state, {
-        predictions: [],
-        selectedLocation: null
-      }));
-  },
-  render: function() {
+  render: function () {
     return (
-      <div className='location-search-container'>
-        <input className="location-search" id="location-search" placeholder="Enter a location"
-          onChange={e => this.updatePredictions(e.target.value)}
-          onFocus={e => {
-            this.updatePredictions(e.target.value);
-          }}
-          onBlur={e => this.removePredictions()} 
-          ref="searchInput" />
-        <ul className="search-result-container">
-          {
-            this.state.predictions.map(p => (
-              <li key={p.place_id} 
-                className="search-result location-search-result"
-                onMouseDown={e => this.selectLocation(p)}>{p.description}</li>
-            ))
-          }
-        </ul>
-      </div>
+      <Search
+        name="location-search"
+        placeholder="Enter a location"
+        onAutoComplete={this.doAutoComplete}
+        />
     )
   }
 });
-
-const doAutoComplete = function(input, callback) {
-  const service = new google.maps.places.AutocompleteService();
-  service.getPlacePredictions ({input: input, types: ['(cities)']}, callback);
-};
