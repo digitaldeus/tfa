7f20c3f6b080558a31c5e7864e356bb418091f6a
Deus Duke <deusduke@gmail.com>
2016-05-22 01:00:18 -0700

    dynamic lat and lng search

--- app/js/flux/SearchStore.js
+++ app/js/flux/SearchStore.js
@@ -10,6 +10,9 @@ class SearchStore extends ReduceStore {
       locationPredictions: [],
       selectedChurch: null,
       selectedLocation: null,
+      currentLocation: null,
+      lat: null,
+      lng: null
     }
   }
 
@@ -37,8 +40,14 @@ class SearchStore extends ReduceStore {
       case 'HIDE_LOCATION_SEARCH':
         return Object.assign({}, state, { locationPredictions: [] });
       case 'SELECT_LOCATION':
+        this._getPlaceDetail(action.location.place_id, (location) => {
+          // store the lat and lng
+          this.currentLat = location.geometry.location.lat();
+          this.currentLng = location.geometry.location.lng();
+          
+          console.log(this.currentLat, this.currentLng);
+        });
         return Object.assign({}, state, {
-          selectedLocation: action.location,
           locationInput: action.location.description,
           locationPredictions: []
         });
@@ -73,7 +82,6 @@ class SearchStore extends ReduceStore {
         input: searchString, types: ['(cities)']
       }, (predictions, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
-
           dispatch({
             type: 'LOCATION_SEARCH_RESULTS',
             data: predictions.map(l => {
@@ -91,19 +99,43 @@ class SearchStore extends ReduceStore {
       this.reduce(this.getState(), { type: 'HIDE_CHURCH_SEARCH' });
     }
   }
-
+  
+  /**
+   * dummy map to allow search to happen
+   */
+  _getDummyMap() {
+    if (!this.dummy_map) {
+      this.dummy_map = new google.maps.Map(document.createElement('div'));
+    }
+    
+    return this.dummy_map;
+  }
+  
+  /**
+   * Get the place detail, run the given callback
+   * @param  {string} place_id
+   * @param  {function} callback
+   */
+  _getPlaceDetail(place_id, callback) {
+    const service = new google.maps.places.PlacesService(this._getDummyMap());
+    service.getDetails({placeId: place_id}, (place, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        callback(place);
+      }
+    });
+  }
+ 
   _updateChurchPredictions(searchString) {
     if (searchString && typeof searchString === "string" && searchString.length > 1) {
       let predictions = [];
       // run the passed in autocomplete function
-      const map = new google.maps.Map(document.createElement('div')); // dummy map to allow search to happen
-      const service = new google.maps.places.PlacesService(map);
+      const service = new google.maps.places.PlacesService(this._getDummyMap());
 
       // run the search
       service.nearbySearch({
         keyword: searchString,
         type: 'church',
-        location: {lat: 37.761, lng: -122.14},
+        location: {lat: this.currentLat, lng: this.currentLng},
         radius: 80500,
       }, (predictions, status) => {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
