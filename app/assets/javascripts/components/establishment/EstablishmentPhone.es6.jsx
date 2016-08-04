086175576ed09d60623f181c52ee3acf82dbeb43
Erik Majlath <erikmajlath@gmail.com>
2016-08-04 11:38:14 +0200

    Add component for phone editing

--- app/assets/javascripts/components/establishment/EstablishmentPhone.es6.jsx
+++ app/assets/javascripts/components/establishment/EstablishmentPhone.es6.jsx
@@ -1,23 +1,49 @@
 class EstablishmentPhone extends React.Component {
 
-  static getStores(){
+  static getStores() {
     return [AppEstablishmentStore];
   }
 
-  static calculateState(){
+  static calculateState() {
     return AppEstablishmentStore.getState();
   }
 
-  componentDidMount(){
-    console.log('Establishment Phonw mounted');
-  }
-
-  render(){
+  render() {
     return (
-      <div className="establishment-phone"></div>
+      <div className="establishment-phone-container">
+        <EstablishmentPhoneInput
+          value={this.state.establishment.phone}
+        />
+      </div>
     );
   }
 
+}
+
+class EstablishmentPhoneInput extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: (props.value || "") };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({ value: props.value || "" });
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  render() {
+    return (
+      <input
+        type="text"
+        value={this.state.value}
+        onChange={this.handleChange.bind(this)}
+      />
+    );
+  }
 }
 
 const EstablishmentPhoneContainer = FluxUtils.Container.create(EstablishmentPhone);
--- app/views/establishments/show.json.jbuilder
+++ app/views/establishments/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @establishment, :id, :name, :description, :created_at, :updated_at
+json.extract! @establishment, :id, :name, :description, :created_at, :updated_at, :phone
